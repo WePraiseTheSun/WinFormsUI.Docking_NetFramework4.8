0613c9279eb0b3d6d85689009da8f6d81efddd0a
Jerry Liang <jerry27syd@hotmail.com>
2024-05-07 02:51:43 +1000

    Add project files.

---
+++ WinformsUI.Themes/ThemeVS2012/VS2012DarkTheme.cs
@@ -0,0 +1,16 @@
+namespace WeifenLuo.WinFormsUI.Docking
+{
+    using ThemeVS2012;
+    using WinformsUI.Themes.ThemeVS2012;
+
+    /// <summary>
+    /// Visual Studio 2012 Dark theme.
+    /// </summary>
+    public class VS2012DarkTheme : VS2012ThemeBase
+    {
+        public VS2012DarkTheme()
+            :base(Decompress(Resources.vs2012dark_vstheme), null, null)
+        {
+        }
+    }
+}
